24dea70601df946e395e939d8e1a82657981b950
quangminh <quangminh@gmail.com>
2023-05-13 04:35:28 +0700

    br05_module

--- src/components/ProgressBar.vue
+++ src/components/ProgressBar.vue
@@ -1,26 +1,12 @@
 <template>
-  <p class="progress-bar" v-if="auth.isAuthenticated">
-    Progress: {{ progress }}%
-  </p>
+  <p class="progress-bar" v-if="isAuthenticated">Progress: {{ progress }}%</p>
 </template>
 
 <script>
-import { mapGetters, mapState } from "vuex";
+import { mapGetters } from "vuex";
 export default {
   name: "ProgressBar",
-  computed: {
-    ...mapGetters(["progress"]),
-    ...mapState(["auth"]),
-  },
-  //   computed: {
-  //     progress() {
-  //       return Math.round(
-  //         (this.$store.state.todos.filter((todo) => todo.completed).length /
-  //           this.$store.state.todos.length) *
-  //           100
-  //       );
-  //     },
-  //   },
+  computed: mapGetters(["progress", "isAuthenticated"]),
 };
 </script>
 
